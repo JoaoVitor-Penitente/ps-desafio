0b1851e4f554fd4a61743e43b6e20cd874d42e89
João Vitor Penitente <joaov.penitente@gmail.com>
2022-03-02 21:07:14 -0300

    feat: Criação do model Categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
+    use HasFactory;
+    
+    protected $table = 'categorias';
+    protected $fillable = [
+        'categoria'
+    ];
 }
