045f003a1bf57b671c8c5cedca2234be56bbec3f
João Vitor Penitente <joaov.penitente@gmail.com>
2022-03-02 21:09:36 -0300

    feat: Criação do ProdutoSeeder

--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Produto;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProdutoSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class ProdutoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Produto::factory()->count(30)->create();
     }
 }
