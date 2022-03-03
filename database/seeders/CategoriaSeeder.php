866b29d8db22a2804a3f4ec5009bcb8e6eae7b8d
João Vitor Penitente <joaov.penitente@gmail.com>
2022-03-02 21:09:22 -0300

    feat: Criação do CategoriaSeeder

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -2,18 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Categoria;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategoriaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        Categoria::factory()->count(30)->create();
     }
 }
