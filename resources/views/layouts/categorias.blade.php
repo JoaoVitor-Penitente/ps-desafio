947eb817374ad67b52e523533c077e71b392edd6
João Vitor Penitente <joaov.penitente@gmail.com>
2022-03-08 16:17:40 -0300

    feat: adição das categorias

---
+++ resources/views/layouts/categorias.blade.php
@@ -0,0 +1,11 @@
+<div class="categoria-container">
+    <ul class ="lista-categoria">
+        <li><a href="/produtos/buscar?_token=VxiVuGTPgDrZeYKWmZcp5ixhyAoPiHYYOPILexyo&search=tenis">Tênis</a></li>
+        <li><a href="/produtos/buscar?_token=VxiVuGTPgDrZeYKWmZcp5ixhyAoPiHYYOPILexyo&search=moletom">Moletom</a></li>
+        <li><a href="/produtos/buscar?_token=VxiVuGTPgDrZeYKWmZcp5ixhyAoPiHYYOPILexyo&search=Camisetas">Camisetas</a></li>
+        <li><a href="/produtos/buscar?_token=VxiVuGTPgDrZeYKWmZcp5ixhyAoPiHYYOPILexyo&search=tenis">Calças</a></li>
+        <li><a href="/produtos/buscar?_token=VxiVuGTPgDrZeYKWmZcp5ixhyAoPiHYYOPILexyo&search=tenis">Bermudas</a></li>
+    </ul>
+
+</div>
+    
